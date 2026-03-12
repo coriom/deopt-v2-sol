1aca378ea925b91017b0b304d940f895299449fe
coriom <coriolanmorel@gmail.com>
2026-03-12 00:06:01 +0000

    12/03

--- src/margin/MarginEngine.sol
+++ src/margin/MarginEngine.sol
@@ -14,13 +14,21 @@ import {MarginEngineOps} from "./MarginEngineOps.sol";
 ///   - insuranceFund
 ///   - feesManager
 ///   - feeRecipient
+///   - guardian
 ///   - params liquidation
 ///   - risk params cache
 contract MarginEngine is MarginEngineOps {
     constructor(address _owner, address registry_, address vault_, address oracle_) {
         _initMarginEngineStorage(_owner, registry_, vault_, oracle_);
 
+        // bootstrap guardian: owner at deploy time.
+        // In production, ownership can later be transferred to timelock
+        // and guardian rotated to a dedicated emergency operator.
+        _setGuardian(_owner);
+
         // defaults explicitement émis au déploiement
         emit LiquidationOracleMaxDelaySet(0, liquidationOracleMaxDelay);
+        emit GlobalPauseSet(false);
+        emit EmergencyModeUpdated(tradingPaused, liquidationPaused, settlementPaused, collateralOpsPaused);
     }
 }
